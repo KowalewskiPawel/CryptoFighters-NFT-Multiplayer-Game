6c08415dc5c9f1f07cc215f2f8712dc2afa6a2ec
KowalewskiPawel <pawelkowalewsk@gmail.com>
2022-01-07 22:56:53 +0100

    boilerplate code added

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x500557e7dd744Bc88003532C0D94d28d1ed52Ee2";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
